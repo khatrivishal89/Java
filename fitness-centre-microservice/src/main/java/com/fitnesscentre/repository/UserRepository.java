bfe81259c585a47193b9570e31371ac8ca277dfe
khatrivishal89 <160641079+khatrivishal89@users.noreply.github.com>
2025-10-14 15:30:43 +0530

    Create UserRepository.java

---
+++ fitness-centre-microservice/src/main/java/com/fitnesscentre/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fitnesscentre.repository;
+
+import com.fitnesscentre.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
